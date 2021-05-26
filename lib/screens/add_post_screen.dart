7469630fee848a4421c8f79ee903cbc205c75316
MotasemX <48732716+MotasemTechcampus@users.noreply.github.com>
2021-05-26 16:04:24 +0300

    loading on post

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -9,11 +9,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:habini/screens/navigation_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 final _auth = FirebaseAuth.instance;
 final _firebase = FirebaseFirestore.instance;
 
 User logedInUser;
+bool showSpinner = false;
 
 class AddPost extends StatefulWidget {
   @override
@@ -73,186 +75,145 @@ class _AddPostState extends State<AddPost> {
           ],
         ),
       ),
-      body: ListView(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.3,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-            ),
-            padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5.0, left: 5.0),
-                      child: CircleAvatar(
-                        backgroundColor: UniformColor,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.2,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextFormField(
-                      controller: postTextController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        hintText: 'whats on your mind?',
-                        border: InputBorder.none,
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: ListView(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5.0, left: 5.0),
+                        child: CircleAvatar(
+                          backgroundColor: UniformColor,
                         ),
                       ),
-                      keyboardType: TextInputType.text,
-                      maxLines: null,
-                      style: TextStyle(color: Colors.black),
-                      cursorColor: Colors.black,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.deny(RegExp(r'[/\\]')),
-                        LengthLimitingTextInputFormatter(170),
-                      ],
-                      onChanged: (value) {
-                        _postContent = value;
-                      }),
-                ),
-                Opacity(
-                  opacity: 0.5,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 0.0),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 1,
-                      height: 0.5,
-                      color: Colors.black,
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextFormField(
+                        controller: postTextController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          hintText: 'whats on your mind?',
+                          border: InputBorder.none,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                        keyboardType: TextInputType.text,
+                        maxLines: null,
+                        style: TextStyle(color: Colors.black),
+                        cursorColor: Colors.black,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.deny(RegExp(r'[/\\]')),
+                          LengthLimitingTextInputFormatter(170),
+                        ],
+                        onChanged: (value) {
+                          _postContent = value;
+                        }),
+                  ),
+                  Opacity(
+                    opacity: 0.5,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 1,
+                        height: 0.5,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text("Set post duration",
-                style: GoogleFonts.koHo(
-                  fontSize: 25,
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text("Set post duration",
+                  style: GoogleFonts.koHo(
+                    fontSize: 25,
+                    color: UniformColor,
+                  )),
+            ),
+            NumberPicker(
+                selectedTextStyle: TextStyle(
                   color: UniformColor,
-                )),
-          ),
-          NumberPicker(
-              selectedTextStyle: TextStyle(
-                color: UniformColor,
-                fontSize: 30,
-              ),
-              value: _defaultPostTime,
-              minValue: 1,
-              maxValue: 24,
-              itemHeight: 120,
-              itemWidth: 120,
-              onChanged: (value) => setState(() => _defaultPostTime = value),
-              axis: Axis.horizontal,
-              textStyle: GoogleFonts.koHo()),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              'Post duration: is $_defaultPostTime hours',
-              style: TextStyle(
-                fontSize: 20,
+                  fontSize: 30,
+                ),
+                value: _defaultPostTime,
+                minValue: 1,
+                maxValue: 24,
+                itemHeight: 120,
+                itemWidth: 120,
+                onChanged: (value) => setState(() => _defaultPostTime = value),
+                axis: Axis.horizontal,
+                textStyle: GoogleFonts.koHo()),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                'Post duration: is $_defaultPostTime hours',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-          ),
-          Material(
-            color: UniformColor,
-            borderRadius: BorderRadius.circular(10.0),
-            elevation: 5.0,
-            child: MaterialButton(
-              onPressed: () {
-                if (logedInUser == null) {
-                  Alert(
-                    context: context,
-                    type: AlertType.error,
-                    title: "Login Alert",
-                    desc: "Please Login First",
-                    buttons: [
-                      DialogButton(
-                        child: Text(
-                          "Cancel",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                        onPressed: () => Navigator.pop(context),
-                        width: 120,
-                      )
-                    ],
-                  ).show();
-                }
-                if (_postContent == null) {
-                  Alert(
-                    context: context,
-                    type: AlertType.warning,
-                    title: "Post Alert",
-                    desc: "Write something to post",
-                    buttons: [
-                      DialogButton(
-                        child: Text(
-                          "Back",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                        onPressed: () => Navigator.pop(context),
-                        width: 120,
-                      ),
-                    ],
-                  ).show();
-                } else {
-                  postTextController.clear();
-
-                  String id = _firebase.collection('Posts').doc().id;
-
-                  try {
-                    _firebase.collection('Posts').doc(id).set({
-                      'content': _postContent.toString(),
-                      'poster': logedInUser.uid,
-                      'sentOn': FieldValue.serverTimestamp(),
-                      'numOfComments': numberOfComments,
-                      'votesNumber': numberOfVotes,
-                      'duration': _defaultPostTime,
-                      'phoneNumber': logedInUser.phoneNumber,
-                      'postId': id,
+            Material(
+              color: UniformColor,
+              borderRadius: BorderRadius.circular(10.0),
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  if (logedInUser == null) {
+                    setState(() {
+                      showSpinner = false;
                     });
-                    /*  Alert(
+                    Alert(
                       context: context,
-                      type: AlertType.success,
-                      title: "Successfully Posted",
-                      desc: "your post is now in the home screen",
+                      type: AlertType.error,
+                      title: "Login Alert",
+                      desc: "Please Login First",
                       buttons: [
                         DialogButton(
                           child: Text(
-                            "Back",
+                            "Cancel",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                           onPressed: () => Navigator.pop(context),
                           width: 120,
-                        ),
+                        )
                       ],
-                    ).show(); */
-                    //
-                    sleep(Duration(milliseconds: 500));
-                    Navigator.pushNamed(context, 'navigation_page');
-                  } catch (ex) {
+                    ).show();
+                  }
+                  if (_postContent == null) {
+                    setState(() {
+                      showSpinner = false;
+                    });
                     Alert(
                       context: context,
-                      type: AlertType.error,
-                      title: "Connection error",
-                      desc: "Please check your internet connection",
+                      type: AlertType.warning,
+                      title: "Post Alert",
+                      desc: "Why so empty... ",
                       buttons: [
                         DialogButton(
                           child: Text(
@@ -264,19 +225,80 @@ class _AddPostState extends State<AddPost> {
                         ),
                       ],
                     ).show();
+                  } else {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    postTextController.clear();
+
+                    String id = _firebase.collection('Posts').doc().id;
+
+                    try {
+                    await  _firebase.collection('Posts').doc(id).set({
+                        'content': _postContent.toString(),
+                        'poster': logedInUser.uid,
+                        'sentOn': FieldValue.serverTimestamp(),
+                        'numOfComments': numberOfComments,
+                        'votesNumber': numberOfVotes,
+                        'duration': _defaultPostTime,
+                        'phoneNumber': logedInUser.phoneNumber,
+                        'postId': id,
+                      });
+                      /*  Alert(
+                        context: context,
+                        type: AlertType.success,
+                        title: "Successfully Posted",
+                        desc: "your post is now in the home screen",
+                        buttons: [
+                          DialogButton(
+                            child: Text(
+                              "Back",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                            width: 120,
+                          ),
+                        ],
+                      ).show(); */
+                      //
+                      // sleep(Duration(milliseconds: 1000));
+                      Navigator.pushNamed(context, 'navigation_page');
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (ex) {
+                      showSpinner = false;
+                      Alert(
+                        context: context,
+                        type: AlertType.error,
+                        title: "Connection error",
+                        desc: "Please check your internet connection",
+                        buttons: [
+                          DialogButton(
+                            child: Text(
+                              "Back",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                            width: 120,
+                          ),
+                        ],
+                      ).show();
+                    }
                   }
-                }
-              },
-              minWidth: 100,
-              height: 42.0,
-              child: Text('Post',
-                  style: GoogleFonts.koHo(
-                    fontSize: 20,
-                    color: Colors.white,
-                  )),
+                },
+                minWidth: 100,
+                height: 42.0,
+                child: Text('Post',
+                    style: GoogleFonts.koHo(
+                      fontSize: 20,
+                      color: Colors.white,
+                    )),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

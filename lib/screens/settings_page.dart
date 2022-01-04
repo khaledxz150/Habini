19fcbdbb9ed405077a4265d46d155f989b9e1748
MotasemX <48732716+MotasemTechcampus@users.noreply.github.com>
2022-01-04 22:58:04 +0200

    Delete user

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:habini/screens/welcome_screen.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../components.dart';
+
+final _firebase = FirebaseFirestore.instance;
 
 class SettingScreen extends StatefulWidget {
   @override
@@ -36,7 +40,6 @@ class _SettingScreenState extends State<SettingScreen> {
       body: SettingsList(
         sections: [
           SettingsSection(
-            
             tiles: [
               SettingsTile(
                 title: 'Profile ',
@@ -55,6 +58,68 @@ class _SettingScreenState extends State<SettingScreen> {
                 leading: Icon(Icons.question_answer),
                 onPressed: (BuildContext context) {},
               ),
+              // SettingsTile(
+              //   title: 'Delete Account',
+              //   leading: Icon(Icons.block_flipped),
+              //   onPressed: (BuildContext context) {
+              //     Alert(
+              //       context: context,
+              //       type: AlertType.warning,
+              //       title: "Delete your Account",
+              //       desc:
+              //           "are you sure you want to delete your account ? , all your posts and comments will be deleted after this action",
+              //       buttons: [
+              //         DialogButton(
+              //           child: Text(
+              //             "Yes",
+              //             style: TextStyle(color: Colors.white, fontSize: 20),
+              //           ),
+              //           onPressed: () async {
+              //             await _firebase
+              //                 .collection('Posts')
+              //                 .doc()
+              //                 .collection('Comments')
+              //                 .where('commenter', isEqualTo: logedInUser.uid)
+              //                 .get()
+              //                 .then(
+              //                   (snapshot) {
+              //                 for (DocumentSnapshot ds in snapshot.docs) {
+              //                   ds.reference.delete();
+              //                 }
+              //               },
+              //             );
+              //             await _firebase
+              //                 .collection('Posts')
+              //                 .where('poster', isEqualTo: logedInUser.uid)
+              //                 .get()
+              //                 .then((snapshot) {
+              //               for (DocumentSnapshot ds in snapshot.docs) {
+              //                 ds.reference.delete();
+              //               }
+              //             });
+              //             try {
+              //               await _firebase
+              //                   .collection('Users')
+              //                   .doc(logedInUser.uid)
+              //                   .delete();
+              //             } catch (e) {
+              //               print(e);
+              //             }
+              //             Navigator.pushAndRemoveUntil(
+              //               context,
+              //               MaterialPageRoute(
+              //                 builder: (BuildContext context) =>
+              //                     WelcomeScreen(),
+              //               ),
+              //               (route) => false,
+              //             );
+              //           },
+              //           color: Color.fromRGBO(0, 179, 134, 1.0),
+              //         ),
+              //       ],
+              //     ).show();
+              //   },
+              // ),
               SettingsTile(
                 title: 'Sign out',
                 leading: Icon(

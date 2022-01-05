6005aa6bbbe218efb51b1a3af6907ff0aade642c
MotasemX <48732716+MotasemTechcampus@users.noreply.github.com>
2022-01-05 19:17:41 +0200

    FAQ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:habini/screens/faq_screen.dart';
 import 'package:habini/screens/profile_screen.dart';
 import 'package:habini/screens/save_user_data.dart';
 import 'package:habini/screens/sign_up_screen.dart';
@@ -38,6 +39,7 @@ class habini extends StatelessWidget {
         'home_screen': (context) => HomeIndex(),
         'comments_screen': (context) => CommentsScreen(),
         'university_number_auth_screen': (context) => UniversityAuth(),
+        'faq_page': (context)=> FAQPage(),
       },
     );
   }
